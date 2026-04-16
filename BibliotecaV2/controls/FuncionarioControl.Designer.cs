ac0a8e7d71decbcac5a9ef38edfb00d0970668f4
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-04-16 10:14:36 -0300

    feat(funcionarios): Criar estrutura inicial do FuncionarioControl

---
+++ BibliotecaV2/controls/FuncionarioControl.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace BibliotecaV2.controls
+{
+    partial class FuncionarioControl
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDataCadastro = new System.Windows.Forms.DateTimePicker();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.btnAtualizarLista = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.lboDados = new System.Windows.Forms.ListBox();
+            this.lblDataCadastro = new System.Windows.Forms.Label();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.txtSenhaHash = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.dtpUltimoLogin = new System.Windows.Forms.DateTimePicker();
+            this.lblUltimoLogin = new System.Windows.Forms.Label();
+            this.chkAtivo = new System.Windows.Forms.CheckBox();
+            this.lblAtivo = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCargos = new System.Windows.Forms.Label();
+            this.cboCargos = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // dtpDataCadastro
+            // 
+            this.dtpDataCadastro.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtpDataCadastro.Enabled = false;
+            this.dtpDataCadastro.Location = new System.Drawing.Point(28, 261);
+            this.dtpDataCadastro.Name = "dtpDataCadastro";
+            this.dtpDataCadastro.Size = new System.Drawing.Size(256, 20);
+            this.dtpDataCadastro.TabIndex = 73;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(28, 335);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 72;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(302, 70);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(98, 13);
+            this.lblBuscar.TabIndex = 71;
+            this.lblBuscar.Text = "Buscar funcionário:";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(305, 86);
+            this.txtPesquisa.Multiline = true;
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(313, 20);
+            this.txtPesquisa.TabIndex = 70;
+            // 
+            // btnAtualizarLista
+            // 
+            this.btnAtualizarLista.Location = new System.Drawing.Point(305, 335);
+            this.btnAtualizarLista.Name = "btnAtualizarLista";
+            this.btnAtualizarLista.Size = new System.Drawing.Size(87, 23);
+            this.btnAtualizarLista.TabIndex = 69;
+            this.btnAtualizarLista.Text = "Atualizar Lista";
+            this.btnAtualizarLista.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(543, 335);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 68;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // lboDados
+            // 
+            this.lboDados.FormattingEnabled = true;
+            this.lboDados.Location = new System.Drawing.Point(305, 117);
+            this.lboDados.Name = "lboDados";
+            this.lboDados.Size = new System.Drawing.Size(313, 212);
+            this.lboDados.TabIndex = 67;
+            // 
+            // lblDataCadastro
+            // 
+            this.lblDataCadastro.AutoSize = true;
+            this.lblDataCadastro.Location = new System.Drawing.Point(25, 245);
+            this.lblDataCadastro.Name = "lblDataCadastro";
+            this.lblDataCadastro.Size = new System.Drawing.Size(78, 13);
+            this.lblDataCadastro.TabIndex = 66;
+            this.lblDataCadastro.Text = "Data Cadastro:";
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(28, 116);
+            this.txtNomeCompleto.Multiline = true;
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(256, 20);
+            this.txtNomeCompleto.TabIndex = 65;
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(25, 100);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(85, 13);
+            this.lblNomeCompleto.TabIndex = 64;
+            this.lblNomeCompleto.Text = "Nome Completo:";
+            // 
+            // txtSenhaHash
+            // 
+            this.txtSenhaHash.Location = new System.Drawing.Point(28, 70);
+            this.txtSenhaHash.Multiline = true;
+            this.txtSenhaHash.Name = "txtSenhaHash";
+            this.txtSenhaHash.Size = new System.Drawing.Size(256, 20);
+            this.txtSenhaHash.TabIndex = 63;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(25, 54);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(69, 13);
+            this.lblSenha.TabIndex = 62;
+            this.lblSenha.Text = "Senha Hash:";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(108, 335);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(176, 23);
+            this.btnSalvar.TabIndex = 61;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(28, 29);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(256, 20);
+            this.txtUsuario.TabIndex = 60;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(25, 13);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 59;
+            this.lblUsuario.Text = "Usuário:";
+            // 
+            // dtpUltimoLogin
+            // 
+            this.dtpUltimoLogin.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtpUltimoLogin.Enabled = false;
+            this.dtpUltimoLogin.Location = new System.Drawing.Point(28, 309);
+            this.dtpUltimoLogin.Name = "dtpUltimoLogin";
+            this.dtpUltimoLogin.Size = new System.Drawing.Size(256, 20);
+            this.dtpUltimoLogin.TabIndex = 75;
+            // 
+            // lblUltimoLogin
+            // 
+            this.lblUltimoLogin.AutoSize = true;
+            this.lblUltimoLogin.Location = new System.Drawing.Point(25, 293);
+            this.lblUltimoLogin.Name = "lblUltimoLogin";
+            this.lblUltimoLogin.Size = new System.Drawing.Size(72, 13);
+            this.lblUltimoLogin.TabIndex = 74;
+            this.lblUltimoLogin.Text = "Último Logon:";
+            // 
+            // chkAtivo
+            // 
+            this.chkAtivo.AutoSize = true;
+            this.chkAtivo.Location = new System.Drawing.Point(305, 29);
+            this.chkAtivo.Name = "chkAtivo";
+            this.chkAtivo.Size = new System.Drawing.Size(43, 17);
+            this.chkAtivo.TabIndex = 76;
+            this.chkAtivo.Text = "Sim";
+            this.chkAtivo.UseVisualStyleBackColor = true;
+            this.chkAtivo.CheckedChanged += new System.EventHandler(this.chkAtivo_CheckedChanged);
+            // 
+            // lblAtivo
+            // 
+            this.lblAtivo.AutoSize = true;
+            this.lblAtivo.Location = new System.Drawing.Point(302, 13);
+            this.lblAtivo.Name = "lblAtivo";
+            this.lblAtivo.Size = new System.Drawing.Size(34, 13);
+            this.lblAtivo.TabIndex = 77;
+            this.lblAtivo.Text = "Ativo:";
+            this.lblAtivo.Click += new System.EventHandler(this.lblAtivo_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(28, 213);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(256, 20);
+            this.txtEmail.TabIndex = 79;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(25, 197);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 78;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblCargos
+            // 
+            this.lblCargos.AutoSize = true;
+            this.lblCargos.Location = new System.Drawing.Point(25, 148);
+            this.lblCargos.Name = "lblCargos";
+            this.lblCargos.Size = new System.Drawing.Size(43, 13);
+            this.lblCargos.TabIndex = 80;
+            this.lblCargos.Text = "Cargos:";
+            // 
+            // cboCargos
+            // 
+            this.cboCargos.FormattingEnabled = true;
+            this.cboCargos.Location = new System.Drawing.Point(28, 164);
+            this.cboCargos.Name = "cboCargos";
+            this.cboCargos.Size = new System.Drawing.Size(256, 21);
+            this.cboCargos.TabIndex = 81;
+            // 
+            // FuncionarioControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cboCargos);
+            this.Controls.Add(this.lblCargos);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblAtivo);
+            this.Controls.Add(this.chkAtivo);
+            this.Controls.Add(this.dtpUltimoLogin);
+            this.Controls.Add(this.lblUltimoLogin);
+            this.Controls.Add(this.dtpDataCadastro);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.btnAtualizarLista);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.lboDados);
+            this.Controls.Add(this.lblDataCadastro);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Controls.Add(this.lblNomeCompleto);
+            this.Controls.Add(this.txtSenhaHash);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "FuncionarioControl";
+            this.Size = new System.Drawing.Size(642, 374);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDataCadastro;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button btnAtualizarLista;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.ListBox lboDados;
+        private System.Windows.Forms.Label lblDataCadastro;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.TextBox txtSenhaHash;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.DateTimePicker dtpUltimoLogin;
+        private System.Windows.Forms.Label lblUltimoLogin;
+        private System.Windows.Forms.CheckBox chkAtivo;
+        private System.Windows.Forms.Label lblAtivo;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCargos;
+        private System.Windows.Forms.ComboBox cboCargos;
+    }
+}
---
+++ BibliotecaV2/controls/FuncionarioControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibliotecaV2.controls
+{
+    public partial class FuncionarioControl : UserControl
+    {
+        public FuncionarioControl()
+        {
+            InitializeComponent();
+        }
+
+        private void lblAtivo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkAtivo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
