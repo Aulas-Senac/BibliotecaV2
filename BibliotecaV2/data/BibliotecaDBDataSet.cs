e826976f6d0a234c5a0ac5e1d3307a7d7a1e78c2
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-04-16 09:37:43 -0300

    db: configurar conexão com banco de dados

---
+++ BibliotecaV2/data/BibliotecaDBDataSet.cs
@@ -0,0 +1,19 @@
+﻿namespace BibliotecaV2.data
+{
+
+
+    partial class BibliotecaDBDataSet
+    {
+        partial class UsuariosDataTable
+        {
+        }
+
+        partial class UsuariosRow
+        {
+            public override string ToString()
+            {
+                return $"{UsuarioID} - {Nome} | {Telefone} | {Email}";
+            }
+        }
+    }
+}
