18399e78ddc8b739677e23b53d54d459554cffd8
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-04-15 22:59:46 -0300

    feat(UsuarioControl): implementar layout do UsuarioControl

--- BibliotecaV2/controls/UsuarioControl.Designer.cs
+++ BibliotecaV2/controls/UsuarioControl.Designer.cs
@@ -28,18 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dtpDataCadastro = new System.Windows.Forms.DateTimePicker();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.btnAtualizarLista = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.lboDados = new System.Windows.Forms.ListBox();
+            this.lblDataCadastro = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // dtpDataCadastro
+            // 
+            this.dtpDataCadastro.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtpDataCadastro.Enabled = false;
+            this.dtpDataCadastro.Location = new System.Drawing.Point(56, 181);
+            this.dtpDataCadastro.Name = "dtpDataCadastro";
+            this.dtpDataCadastro.Size = new System.Drawing.Size(256, 20);
+            this.dtpDataCadastro.TabIndex = 58;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(56, 221);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 57;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(330, 16);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(80, 13);
+            this.lblBuscar.TabIndex = 56;
+            this.lblBuscar.Text = "Buscar usuário:";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(333, 32);
+            this.txtPesquisa.Multiline = true;
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(406, 20);
+            this.txtPesquisa.TabIndex = 55;
+            // 
+            // btnAtualizarLista
+            // 
+            this.btnAtualizarLista.Location = new System.Drawing.Point(333, 341);
+            this.btnAtualizarLista.Name = "btnAtualizarLista";
+            this.btnAtualizarLista.Size = new System.Drawing.Size(87, 23);
+            this.btnAtualizarLista.TabIndex = 54;
+            this.btnAtualizarLista.Text = "Atualizar Lista";
+            this.btnAtualizarLista.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(664, 341);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 53;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // lboDados
+            // 
+            this.lboDados.FormattingEnabled = true;
+            this.lboDados.Location = new System.Drawing.Point(333, 58);
+            this.lboDados.Name = "lboDados";
+            this.lboDados.Size = new System.Drawing.Size(406, 277);
+            this.lboDados.TabIndex = 52;
+            // 
+            // lblDataCadastro
+            // 
+            this.lblDataCadastro.AutoSize = true;
+            this.lblDataCadastro.Location = new System.Drawing.Point(53, 164);
+            this.lblDataCadastro.Name = "lblDataCadastro";
+            this.lblDataCadastro.Size = new System.Drawing.Size(78, 13);
+            this.lblDataCadastro.TabIndex = 51;
+            this.lblDataCadastro.Text = "Data Cadastro:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(56, 130);
+            this.txtTelefone.Multiline = true;
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(256, 20);
+            this.txtTelefone.TabIndex = 50;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(53, 114);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(52, 13);
+            this.lblTelefone.TabIndex = 49;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(56, 80);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(256, 20);
+            this.txtEmail.TabIndex = 48;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(53, 64);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 47;
+            this.lblEmail.Text = "Email:";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(136, 221);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(176, 23);
+            this.btnSalvar.TabIndex = 46;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(56, 32);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(256, 20);
+            this.txtNome.TabIndex = 45;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(53, 16);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 44;
+            this.lblNome.Text = "Nome:";
             // 
             // UsuarioControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dtpDataCadastro);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.btnAtualizarLista);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.lboDados);
+            this.Controls.Add(this.lblDataCadastro);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
             this.Name = "UsuarioControl";
             this.Size = new System.Drawing.Size(800, 380);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDataCadastro;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button btnAtualizarLista;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.ListBox lboDados;
+        private System.Windows.Forms.Label lblDataCadastro;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
     }
 }
